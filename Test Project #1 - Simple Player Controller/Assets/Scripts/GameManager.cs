3a38b750239954e12a97d07ab3a41ac4b36081a6
Ninjagoldfinch <101228347+NinjaGoldfinch@users.noreply.github.com>
2023-05-31 12:40:22 +1200

    Commit 2: Test #1 - Simple Player Controller
    I have improved the camera to fit my test background, to allow for better grahpics and movement.

--- Test Project #1 - Simple Player Controller/Assets/Scripts/GameManager.cs
+++ Test Project #1 - Simple Player Controller/Assets/Scripts/GameManager.cs
@@ -7,10 +7,12 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     
     public Camera cam;
-    
+    public int camsize = 100;
+
     void Start()
     {
-        cam.pixelRect = new Rect(0,0,1000,1000);
+        cam.orthographicSize = camsize;
+
     }
 
     // Update is called once per frame
